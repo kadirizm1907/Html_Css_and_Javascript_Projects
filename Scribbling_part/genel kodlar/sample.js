ee4ad690d96baeba45d71abd4293294c5423f06a
Kadir_yurt <kadiryurt1907@gmail.com>
2023-10-23 20:01:36 +0200

    reduce, find

--- Scribbling_part/genel kodlar/sample.js
+++ Scribbling_part/genel kodlar/sample.js
@@ -8,3 +8,31 @@ const numbers = [ 1, 2, 3, 45, 65 ];
   const average =sum / numbers.length;
   console.log(average);
 
+
+
+
+
+
+const salary = [10000, 15000, 9000, 20000]
+const sumOfsalary = salary.reduce((sum,sal) => sum + sal, 0)
+console.log("total:", sumOfsalary)
+
+let result 
+const sumOfsalary1 = salary.reduce((sum,sal, i) =>{
+  result = `${i} ${sum + sal}`
+  console.log(result)
+ return sum + sal
+} )
+console.log("total:", sumOfsalary1)
+
+
+const multply = salary.reduce((acc, val) => acc * val, 1)
+console.log(multply)
+
+
+
+//? maaslari 10000 ile 15 000 arasinda olanlarin maaslarini bulup yuzde 20 i zam yapip maaslarin toplamini al .. filter-map-reduce
+
+
+const zamliMaasToplami = salary.filter((s) => s>= 9000 && s < 15000).map((s) => s * 1.2).reduce((sum,salaries) => sum + salaries)
+console.log("Zamli maas toplami =", zamliMaasToplami)
