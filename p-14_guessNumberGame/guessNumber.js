efdbb4673eb01e7740c061eb77f4c21255c1f139
kadirizm1907 <kadiryurt1907@gmail.com>
2023-11-04 09:05:23 +0100

    guess

--- p-14_guessNumberGame/guessNumber.js
+++ p-14_guessNumberGame/guessNumber.js
@@ -1,15 +1,36 @@
+console.log("welcome to guess game");
 
+let sayi = Math.round(Math.random() * 100);
+console.log(sayi);
 
-let rastgeleSayi = Math.ceil(Math.random() * 10) 
-document.getElementById("gonder").onclick = function(){
-    let yazilanSayi = document.getElementById("bizyazdik")
-    if (yazilanSayi == rastgeleSayi){
-        alert("tebrikler")
+let sonucEl = document.getElementById("sonuc");
+let listeEl = document.getElementById("liste");
+let tahminInput = document.getElementById("tahminInput");
+let tahminButton = document.getElementById("tahminButton");
+
+let count = 0;
+
+tahminButton.addEventListener("click", function () {
+  let num1 = parseInt(tahminInput.value);
+  let li = document.createElement("li");
+
+  if (num1 === sayi) {
+    li.textContent = "Girdiğiniz sayı: " + num1 + " Doğru bildiniz!!!";
+    listeEl.appendChild(li);
+    tahminInput.disabled = true;
+    tahminButton.disabled = true;
+  } else {
+    if (num1 < sayi) {
+      li.textContent = "---Girdiğiniz sayı: " + num1 + " Arttırınız!!!" + " " + (4 - count) + " hakkınız kaldı!!!" ;
+    } else {
+      li.textContent = "---Girdiğiniz sayı: " + num1 + " Azaltınız!!!" + " " +  (4 - count) + " hakkınız kaldı!!!";
     }
-        else if(yazilanSayi > rastgeleSayi){
-            alert("kucuk deger yaz")
-        }
-        else{
-            alert("buyuk deger yaz")
-        }
+    listeEl.appendChild(li);
+    count++;
+    if (count === 5) {
+      sonucEl.textContent = "5 tahmin hakkınız bitti. Doğru cevap: " + sayi;
+      tahminInput.disabled = true;
+      tahminButton.disabled = true;
     }
+  }
+});
