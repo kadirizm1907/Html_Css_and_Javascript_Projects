1b5f0cec38e33032911d512efa0675c4f0bbc858
kadirizm1907 <kadiryurt1907@gmail.com>
2023-11-11 00:27:10 +0100

    a

--- p-17_colorChange/script.js
+++ p-17_colorChange/script.js
@@ -1,26 +1,16 @@
 
 
-// const color = ["red", "blue", "green", "yellow", "black", "pink", "purple", "brown"];
-const bgColor = document.getElementById('women');
-// const randomNum = Math.floor(Math.random() * color.length);
-
-// document.querySelector("#women").style.color = color[randomNum];
-function randomColor(){
-    return Math.floor(Math.random()* 255);
-};
-bgColor.addEventListener('click', () => {
-    bgColor.style.backgroundColor = 'rgba('+randomColor()+', '+randomColor()+', '+randomColor()+', '+randomColor()+', '+randomColor()+', '+randomColor()+', '+randomColor()+')'
-});
-// document.getElementsByClassName("btn btnBg") = bgColor[randomNum];
+const color = ["red","blue","green","yellow","black","pink","purple","brown"];
+const randomNum = Math.floor(Math.random() * color.length);
 
 
+// function randomColor(){
+// document.querySelector('main').style.backgroundColor = color[randomNum];
+// };
+document.querySelector('#btnBg').addEventListener('click', () => {
+    const randomNum = Math.floor(Math.random() * color.length);
+    const color = ["red","blue","green","yellow","black","pink","purple","brown"];
+    document.querySelector('#main').style.backgroundColor = color[randomNum];
+    
 
-// document.querySelector("#clr1").addEventListener('click', () =>{
-// const color = ["red", "blue", "green", "yellow", "black", "pink", "purple", "brown"];
-
-// const randomNum = Math.floor(Math.random() * color.length);
-
-
-// document.getElementById("#women").style.backgroundColor = color[randomNum];
-
-// })
+})
