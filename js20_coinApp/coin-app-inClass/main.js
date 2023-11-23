72106f55db0020201fc7a784b45e7cfe1667a345
kadirizm1907 <kadiryurt1907@gmail.com>
2023-11-23 17:48:37 +0100

    engver

--- js20_coinApp/coin-app-inClass/main.js
+++ js20_coinApp/coin-app-inClass/main.js
@@ -1,1 +1,23 @@
-import "./scss/style.scss";
+//? Projenin style dosyasini ana js (main.js) kodumuza import ettik.
+import "./scss/style.scss"
+import { apiRequest } from "./src/apiRequest.js"
+
+//? Selector
+const form = document.querySelector("header form")
+
+//? Form için submit event'nin tanimlanmasi
+form.addEventListener("submit", (e) => {
+  console.log(e.target)
+  e.preventDefault() //? tüm submit davranıslarini engelle
+  getCoinData() //? API'ye istek at.
+  e.target.reset() //? formu silme davranısi calisitir
+})
+
+const getCoinData = () => {
+  const input = document.querySelector("header form input").value
+  if (!input.trim()) {
+    alert("Input can not be blank ")
+  } else {
+    apiRequest(input)
+  }
+}
